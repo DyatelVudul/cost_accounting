c140ca206fcf9e734f303795c32d856dd8d4a622
DyatelVudul <shkoliar1470@gmail.com>
2024-05-12 22:14:22 +0200

    Implemented LongHolder and tested it

---
+++ src/main/java/LongHolder.java
@@ -0,0 +1,15 @@
+public class LongHolder {
+    private long value;
+
+    public LongHolder(long value) {
+        this.value = value;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+}
