77ec0ab543b11c8e449e5371444657aae5cc331b
DyatelVudul <shkoliar1470@gmail.com>
2024-06-19 10:57:08 +0200

    Fixed LIFO implementation

--- src/main/java/LIFO.java
+++ src/main/java/LIFO.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class LIFO {
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class LIFO implements QueueSolver{
+
+    @Override
+    public Queue<Tuple<Long, Double>> processEvents(Collection<Event> events, long initialCapacity, double initialPrice) {
+        if  (events == null || events.isEmpty()) {
+            return null;
+        }
+
+        Queue<Tuple<Long, Double>> result = new LinkedList<>();
+        long inCapacity = initialCapacity;
+
+        Stack<Event> purchases = new Stack<>();
+
+        for (Event event : events) {
+            if (event.getType() == EventType.PURCHASE){
+                purchases.push(event);
+                continue;
+            }
+
+            if (event.getType() == EventType.CONSUMPTION){
+                while (true){
+                    if (purchases.isEmpty() || event.getQuantity() == 0){ // If Consumption need 0 units or no more Purchases are available
+                        break;
+                    }
+
+                    Event purchase = purchases.peek();
+
+                    result.add(new Tuple<>(purchase.getQuantity(), purchase.getPrice()));
+
+                    if (purchase.getQuantity() >= event.getQuantity()){ // If Purchase can satisfy Consumption
+                        purchase.setQuantity(purchase.getQuantity() - event.getQuantity());
+                        event.setQuantity(0);
+                        break;
+                    }else { // If Purchase cannot satisfy Consumption
+                        event.setQuantity(event.getQuantity() - purchase.getQuantity());
+                        purchases.pop();
+                    }
+                }
+
+                if(event.getQuantity() > 0){ // Case if none of the purchases can satisfy Consumption
+                    if(event.getQuantity() > inCapacity){
+                        throw new RuntimeException("Cannot satisfy consumption: No more units are available");
+                    }
+
+                    inCapacity -= event.getQuantity();
+                    result.add(new Tuple<>(event.getQuantity(), initialPrice));
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/java/LIFOTest.java
+++ src/test/java/LIFOTest.java
@@ -1,2 +1,51 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LIFOTest {
+
+    private final static Queue<Event> events = new LinkedList<>();
+    private final LIFO solver = new LIFO();
+    private final static Queue<Tuple<Long,Double>> referenceSolution = new LinkedList<>();
+    private static final long INITIAL_CAPACITY = 9780L;
+    private static final float INITIAL_PRICE = 7.1F;
+
+    @BeforeAll
+    public static void setUp() {
+        events.add(new Event(1520, 7.3));
+        events.add(new Event(1030));
+        events.add(new Event(700));
+        events.add(new Event(840, 7.25));
+        events.add(new Event(1360, 7.65));
+        events.add(new Event(580));
+        events.add(new Event(950));
+
+        referenceSolution.add(new Tuple<>(1030L, 7.3));
+        referenceSolution.add(new Tuple<>(490L, 7.3));
+        referenceSolution.add(new Tuple<>(210L, 7.1));
+        referenceSolution.add(new Tuple<>(580L, 7.65));
+        referenceSolution.add(new Tuple<>(780L, 7.65));
+        referenceSolution.add(new Tuple<>(170L, 7.25));
+    }
+
+    @Test
+    public void testExPostAverage(){
+        Queue<Tuple<Long, Double>> processes = solver.processEvents(events, INITIAL_CAPACITY, INITIAL_PRICE);
+        assertEquals(referenceSolution.size(), processes.size());
+    }
+
+    @Test
+    public void testWhenNull(){
+        assertNull(solver.processEvents(null, INITIAL_CAPACITY, INITIAL_PRICE));
+    }
+
+    @Test
+    public void testWhenEmpty(){
+        Queue<Event> empty = new LinkedList<>();
+        assertNull(solver.processEvents(empty, INITIAL_CAPACITY, INITIAL_PRICE));
+    }
 }
