030c437fed18dfe2b158f0b71a1e691f703ba777
DyatelVudul <shkoliar1470@gmail.com>
2024-06-19 11:31:01 +0200

    Fixed FIFO implementation and created basic test

--- src/main/java/FIFO.java
+++ src/main/java/FIFO.java
@@ -1,22 +1,52 @@
 import java.util.*;
 
-public class FIFO implements Solver{
+
+public class FIFO implements QueueSolver {
+
     @Override
-    public float calculatePrice(Collection<Event> events, long initialCapacity, float initialPrice) {
-        // TODO:
-        LinkedList<Event> purchases = new LinkedList<>();
+    public Queue<Tuple<Long, Double>> processEvents(Collection<Event> events, long initialCapacity, double initialPrice) throws RuntimeException {
+        if (events == null || events.isEmpty()) {
+            return null;
+        }
 
-        for (Event event: events){
-            if(event.getType() == EventType.PURCHASE){ // Process purchase
-                purchases.addLast(event);
-                System.out.println("Purchased " + event.getQuantity() + "kg for a price " + event.getPrice() + " Euro.");
-            } else { // Process consumption
-                while (event.getQuantity() > 0){
-                    Event tmp = purchases.getFirst();
+        Queue<Tuple<Long, Double>> result = new LinkedList<>();
 
+        Queue<Event> purchases = new LinkedList<>();
+
+        purchases.add(new Event(initialCapacity, initialPrice));
+
+        for (Event event : events) {
+            if (event.getType() == EventType.PURCHASE){
+                purchases.add(event);
+                continue;
+            }
+
+            if (event.getType() == EventType.CONSUMPTION){
+                while (true){
+                    if (purchases.isEmpty() && event.getQuantity() > 0){ // If Consumption need 0 units or no more Purchases are available
+                        throw new RuntimeException("Not enough inventory to process all purchase events");
+                    }
+
+                    Event purchase = purchases.peek();
+
+                    if(purchase == null){
+                        throw new RuntimeException("Purchase event is null");
+                    }
+
+                    result.add(new Tuple<>(Math.min(purchase.getQuantity(), event.getQuantity()), purchase.getPrice()));
+
+                    if (purchase.getQuantity() >= event.getQuantity()){ // If Purchase can satisfy Consumption
+                        purchase.setQuantity(purchase.getQuantity() - event.getQuantity());
+                        event.setQuantity(0);
+                        break;
+                    } else { // If Purchase cannot satisfy Consumption
+                        event.setQuantity(event.getQuantity() - purchase.getQuantity());
+                        purchases.remove();
+                    }
                 }
             }
         }
-        return 0;
+
+        return result;
     }
 }
